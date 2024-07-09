0e77ef5228747c80d7a7f6535d59048b4b50b2d6
ayusohm432 <ayushkumar76174@gmail.com>
2024-07-09 17:02:50 +0530

    learnt

---
+++ 3.Array/1.ArrayDeclaration.c
@@ -0,0 +1,22 @@
+//Array is a vector data structure
+
+#include<stdio.h>
+
+int main(){
+
+    //Declare array A with Garbage value
+    int A[5];  
+
+    //Declaring Array with elements
+    int B[5] = {10,20,30,40,50};
+
+    //Declare array of size 5 with given elements and remaining as 0
+    int c[5] = {10,20};
+
+    //Declare aarray with size 5 and all elements with 0
+    int D[5] = {0};
+
+    //Declare array of size as many element in it,
+    int E[] = {10,20,30,40,50,60};
+    
+}
---
+++ 3.Array/2.AccessingArrayElements.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    int A[] = {10,20,30,40,50,60};
+
+    //Accessing 3rd element of array
+
+    printf("%d\n",A[2]);
+    printf("%d\n",2[A]);
+    printf("%d\n",*(A+2));
+
+}
